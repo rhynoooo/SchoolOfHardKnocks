a946ed28028ad107fdec061072f909a6a63a08f8
Chris Kottmyer <ckottmyersalesforce.com>
2017-03-02 00:43:41 -0500

    added headers

--- automation/webscraper/web_scraper_monitor.sh
+++ automation/webscraper/web_scraper_monitor.sh
@@ -2,15 +2,21 @@
 
 function main
 {
+	echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
 	echo "`date +"%Y-%m-%d %H:%M:%S"` ==== Adding to master" 
+	echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
 	git pull origin master 
 	check_err="$?"
 	if [ $check_err -ne "0" ]
 	then
-		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== error occurred, exiting"
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== Failed Update" 
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
 		exit 1
 	else 
-		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== succeeded in updating..."
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== Succeeded Update" 
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ====================="
 		return 0
 	fi	
 }
