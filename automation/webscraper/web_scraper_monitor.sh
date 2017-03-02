1a96464033b10a8a261566442e5c450678627a2e
Chris Kottmyer <ckottmyersalesforce.com>
2017-03-02 00:41:16 -0500

    updating script

--- automation/webscraper/web_scraper_monitor.sh
+++ automation/webscraper/web_scraper_monitor.sh
@@ -1,2 +1,18 @@
-echo "Adding to master" 
-git pull origin master 
+#!/bin/bash
+
+function main
+{
+	echo "`date +"%Y-%m-%d %H:%M:%S"` ==== Adding to master" 
+	git pull origin master 
+	check_err="$?"
+	if [ $check_err -ne "0" ]
+	then
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== error occurred, exiting"
+		exit 1
+	else 
+		echo "`date +"%Y-%m-%d %H:%M:%S"` ==== succeeded in updating..."
+		return 0
+	fi	
+}
+
+main 
